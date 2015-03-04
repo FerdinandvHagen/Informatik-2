da1ac236bdb37573e49dd7abf1cdfc7029293b31
Ferdinand von Hagen <ferdinand.vonhagen@gmail.com>
2015-03-04 06:47:28 +0100

    Corrected Mult to fit the Exercise

--- src/u1/Mult.java
+++ src/u1/Mult.java
@@ -9,16 +9,16 @@ public class Mult {
      * @throws IllegalArgumentException Zahlen sind keine natürlichen Zahlen
      */
     private static int f(int a, int b) throws IllegalArgumentException {
-        if (a < 1 || b < 1) {
-            throw new IllegalArgumentException();
-        }
-
         if (b == 1) return a;
         if (b % 2 == 0) return f(2 * a, b / 2);
         else return a + f(2 * a, b / 2);
     }
 
     public static int mult(int a, int b) {
+        if (a < 1 || b < 1) {
+            throw new IllegalArgumentException();
+        }
+        
         return f(a, b);
     }
 }
